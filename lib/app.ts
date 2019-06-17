b4846d2f1752374caafc7319977d8eec88f4d102
Glendon Philipp <gpbaculio@gmail.com>
2019-06-18 03:18:25 +0800

    added express session

--- lib/app.ts
+++ lib/app.ts
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 var cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
 const uuidv1 = require('uuid/v1');
 require('dotenv').config();
 
@@ -10,6 +11,14 @@ require('dotenv').config();
 
 import ConvertRoute from './routes/convert';
 import FccTestingRoute from './routes/fcc-testing';
+
+interface sessionConfigType {
+  secret: string;
+  genid: () => string;
+  cookie: { secure?: boolean };
+  resave: boolean;
+  saveUninitialized: boolean;
+}
 
 class App {
   public app: express.Application = express();
@@ -19,12 +28,27 @@ class App {
     this.app.use(helmet());
     this.app.use(helmet.noSniff()); // prevent client to guess(sniff) the MIME type.
     this.app.use(helmet.xssFilter());
+    // secure cookies with express-session
+    const sessionConfig: sessionConfigType = {
+      secret: process.env.SECRET_KEY,
+      genid: () => uuidv1(),
+      cookie: {},
+      resave: true,
+      saveUninitialized: true
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+      this.app.set('trust proxy', 1); // trust first proxy
+      sessionConfig.cookie.secure = true; // serve secure cookies
+    }
+
+    this.app.use(session(sessionConfig));
     this.app.use(
       bodyParser.urlencoded({
         extended: true
       })
     );
-    this.app.use(cors({ origin: '*' }));
+    this.app.use(cors({ optionSuccessStatus: 200, origin: '*' }));
     this.app.use(bodyParser.json());
 
     this.app.use(express.static(path.join(__dirname, '..', 'public')));
